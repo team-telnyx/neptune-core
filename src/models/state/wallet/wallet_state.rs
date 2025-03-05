2742072a06343dc759fba6128bd075d08b5158e6
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-03-05 15:10:42 +0100

    chore: Remove explicit `.iter()` from for-loops
    changelog: ignore

--- src/models/state/wallet/wallet_state.rs
+++ src/models/state/wallet/wallet_state.rs
@@ -604,7 +604,7 @@ impl WalletState {
         &mut self,
         expected_utxos: impl IntoIterator<Item = ExpectedUtxo>,
     ) {
-        for expected_utxo in expected_utxos.into_iter() {
+        for expected_utxo in expected_utxos {
             self.add_expected_utxo(ExpectedUtxo::new(
                 expected_utxo.utxo,
                 expected_utxo.sender_randomness,
@@ -1340,7 +1340,7 @@ impl WalletState {
         }
 
         // write UTXO-recovery data to disk.
-        for item in incoming_utxo_recovery_data_list.into_iter() {
+        for item in incoming_utxo_recovery_data_list {
             self.store_utxo_ms_recovery_data(item).await?;
         }
 
@@ -1505,7 +1505,7 @@ impl WalletState {
             .flat_map(|(_txkid, tx_inputs)| tx_inputs.iter())
             .map(|(_, absi, _)| *absi)
             .collect();
-        for (wallet_status_element, membership_proof) in wallet_status.synced_unspent.iter() {
+        for (wallet_status_element, membership_proof) in &wallet_status.synced_unspent {
             // Don't allocate more than needed
             if allocated_amount >= total_spend {
                 break;
