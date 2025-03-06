4b79346f82497d26fcbde9ffcdf6a6138b570dcb
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-03-06 11:33:37 +0100

    chore: Declare items before statements
    Items live for the entire scope they are declared in. But statements are
    processed in order. This might cause confusion as it's hard to figure
    out which item is meant in a statement.
    
    changelog: ignore

--- src/models/state/wallet/wallet_state.rs
+++ src/models/state/wallet/wallet_state.rs
@@ -170,6 +170,12 @@ impl WalletState {
         &self,
         utxo_ms_recovery_data: IncomingUtxoRecoveryData,
     ) -> Result<()> {
+        // Create JSON string ending with a newline as this flushes the write
+        #[cfg(windows)]
+        const LINE_ENDING: &str = "\r\n";
+        #[cfg(not(windows))]
+        const LINE_ENDING: &str = "\n";
+
         #[cfg(test)]
         {
             tokio::fs::create_dir_all(self.wallet_directory_path.clone()).await?;
@@ -182,12 +188,6 @@ impl WalletState {
             .open(self.incoming_secrets_path())
             .await?;
         let mut incoming_secrets_file = BufWriter::new(incoming_secrets_file);
-
-        // Create JSON string ending with a newline as this flushes the write
-        #[cfg(windows)]
-        const LINE_ENDING: &str = "\r\n";
-        #[cfg(not(windows))]
-        const LINE_ENDING: &str = "\n";
 
         let mut json_string = serde_json::to_string(&utxo_ms_recovery_data)?;
         json_string.push_str(LINE_ENDING);
@@ -230,8 +230,9 @@ impl WalletState {
         wallet_secret: WalletSecret,
         cli_args: &Args,
     ) -> Self {
-        // Create or connect to wallet block DB
+        const NUM_PREMINE_KEYS: usize = 10;
 
+        // Create or connect to wallet block DB
         DataDirectory::create_dir_if_not_exists(&data_dir.wallet_database_dir_path())
             .await
             .unwrap();
@@ -301,7 +302,6 @@ impl WalletState {
         // For premine UTXOs there is an additional complication: we do not know
         // the derivation index with which they were derived. So we derive a few
         // keys to have a bit of margin.
-        const NUM_PREMINE_KEYS: usize = 10;
         let premine_keys = (0..NUM_PREMINE_KEYS)
             .map(|n| {
                 wallet_state
